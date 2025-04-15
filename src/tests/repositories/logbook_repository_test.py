79731c0f4d18a0439d2c447f5ab167bbe3b870fb
ilmari21 <juuso.u67@gmail.com>
2025-04-15 13:43:17 +0300

    Creating unit tests for repositories

--- src/tests/repositories/logbook_repository_test.py
+++ src/tests/repositories/logbook_repository_test.py
@@ -7,3 +7,33 @@ class TestLogbookRepository(unittest.TestCase):
     def setUp(self):
         self._logbook_repository = LogbookRepository()
         self._logbook_repository.clear()
+
+    def test_flight_creation(self):
+        test_flight = Flight("teuvo", "EFHK", "EFTP", "12:00", "13:30")
+        created_flight = self._logbook_repository.create(test_flight)
+        self.assertEqual(test_flight.departure, created_flight.departure)
+        self.assertEqual(test_flight.arrival, created_flight.arrival)
+        self.assertEqual(test_flight.dep_time, created_flight.dep_time)
+        self.assertEqual(test_flight.arr_time, created_flight.arr_time)
+
+    def test_find_flights(self):
+        test_flight = Flight("teuvo", "EFHK", "EFTP", "12:00", "13:30")
+        self._logbook_repository.create(test_flight)
+        flight_search = self._logbook_repository.find_by_user(test_flight.pilot)
+        self.assertEqual(len(flight_search), 1)
+        self.assertEqual(flight_search[0].pilot, test_flight.pilot)
+        self.assertEqual(flight_search[0].departure, test_flight.departure)
+        self.assertEqual(flight_search[0].arrival, test_flight.arrival)
+        self.assertEqual(flight_search[0].dep_time, test_flight.dep_time)
+        self.assertEqual(flight_search[0].arr_time, test_flight.arr_time)
+
+    def test_find_flights_when_none_added(self):
+        flight_search = self._logbook_repository.find_by_user("teuvo")
+        self.assertEqual(len(flight_search), 0)
+
+    def test_clear_repository(self):
+        test_flight = Flight("teuvo", "EFHK", "EFTP", "12:00", "13:30")
+        self._logbook_repository.create(test_flight)
+        self._logbook_repository.clear()
+        flight_search = self._logbook_repository.find_by_user(test_flight.pilot)
+        self.assertEqual(len(flight_search), 0)
--- src/tests/repositories/user_repository_test.py
+++ src/tests/repositories/user_repository_test.py
@@ -10,6 +10,23 @@ class TestUserRepository(unittest.TestCase):
 
     def test_user_registration(self):
         test_user = User("teuvo", "testi")
+        created_user = self._user_repository.create(test_user)
+        self.assertEqual(test_user.username, created_user.username)
+        self.assertEqual(test_user.password, created_user.password)
+
+    def test_find_user(self):
+        test_user = User("teuvo", "testi")
         self._user_repository.create(test_user)
-        user_find = self._user_repository.find_user(test_user.username)
-        self.assertEqual(test_user.username, user_find.username)
+        user_search = self._user_repository.find_user("teuvo")
+        self.assertEqual(test_user.username, user_search.username)
+        self.assertEqual(test_user.password, user_search.password)
+
+    def test_find_not_existing_user(self):
+        user_search = self._user_repository.find_user("kalevi")
+        self.assertIsNone(user_search)
+
+    def test_clear_repository(self):
+        test_user = User("teuvo", "testi")
+        self._user_repository.create(test_user)
+        self._user_repository.clear()
+        self.assertIsNone(self._user_repository.find_user("teuvo"))
