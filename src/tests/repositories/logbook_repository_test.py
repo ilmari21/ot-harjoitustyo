6be56fc18228e00e8b15c9a816cb5a7a79477786
ilmari21 <juuso.u67@gmail.com>
2025-04-29 12:02:19 +0300

    Autopep8 formatting

--- src/tests/repositories/logbook_repository_test.py
+++ src/tests/repositories/logbook_repository_test.py
@@ -22,12 +22,18 @@ class TestLogbookRepository(unittest.TestCase):
         test_flight = Flight(test_flight_info)
         created_flight = self._logbook_repository.create(test_flight)
         self.assertEqual(test_flight_info.get('pilot'), created_flight.pilot)
-        self.assertEqual(test_flight_info.get('aircraft_type'), created_flight.aircraft_type)
-        self.assertEqual(test_flight_info.get('aircraft_reg'), created_flight.aircraft_reg)
-        self.assertEqual(test_flight_info.get('departure'), created_flight.departure)
-        self.assertEqual(test_flight_info.get('arrival'), created_flight.arrival)
-        self.assertEqual(test_flight_info.get('dep_time'), created_flight.dep_time)
-        self.assertEqual(test_flight_info.get('arr_time'), created_flight.arr_time)
+        self.assertEqual(test_flight_info.get(
+            'aircraft_type'), created_flight.aircraft_type)
+        self.assertEqual(test_flight_info.get('aircraft_reg'),
+                         created_flight.aircraft_reg)
+        self.assertEqual(test_flight_info.get(
+            'departure'), created_flight.departure)
+        self.assertEqual(test_flight_info.get(
+            'arrival'), created_flight.arrival)
+        self.assertEqual(test_flight_info.get(
+            'dep_time'), created_flight.dep_time)
+        self.assertEqual(test_flight_info.get(
+            'arr_time'), created_flight.arr_time)
 
     def test_find_flights(self):
         test_flight_info = {
@@ -42,15 +48,22 @@ class TestLogbookRepository(unittest.TestCase):
 
         test_flight = Flight(test_flight_info)
         self._logbook_repository.create(test_flight)
-        flight_search = self._logbook_repository.find_by_user(test_flight_info['pilot'])
+        flight_search = self._logbook_repository.find_by_user(
+            test_flight_info['pilot'])
         self.assertEqual(len(flight_search), 1)
         self.assertEqual(flight_search[0].pilot, test_flight_info.get('pilot'))
-        self.assertEqual(flight_search[0].aircraft_type, test_flight_info.get('aircraft_type'))
-        self.assertEqual(flight_search[0].aircraft_reg, test_flight_info.get('aircraft_reg'))
-        self.assertEqual(flight_search[0].departure, test_flight_info.get('departure'))
-        self.assertEqual(flight_search[0].arrival, test_flight_info.get('arrival'))
-        self.assertEqual(flight_search[0].dep_time, test_flight_info.get('dep_time'))
-        self.assertEqual(flight_search[0].arr_time, test_flight_info.get('arr_time'))
+        self.assertEqual(
+            flight_search[0].aircraft_type, test_flight_info.get('aircraft_type'))
+        self.assertEqual(
+            flight_search[0].aircraft_reg, test_flight_info.get('aircraft_reg'))
+        self.assertEqual(flight_search[0].departure,
+                         test_flight_info.get('departure'))
+        self.assertEqual(flight_search[0].arrival,
+                         test_flight_info.get('arrival'))
+        self.assertEqual(flight_search[0].dep_time,
+                         test_flight_info.get('dep_time'))
+        self.assertEqual(flight_search[0].arr_time,
+                         test_flight_info.get('arr_time'))
 
     def test_find_flights_when_none_added(self):
         flight_search = self._logbook_repository.find_by_user("teuvo")
@@ -70,5 +83,6 @@ class TestLogbookRepository(unittest.TestCase):
         test_flight = Flight(test_flight_info)
         self._logbook_repository.create(test_flight)
         self._logbook_repository.clear()
-        flight_search = self._logbook_repository.find_by_user(test_flight_info['pilot'])
+        flight_search = self._logbook_repository.find_by_user(
+            test_flight_info['pilot'])
         self.assertEqual(len(flight_search), 0)
